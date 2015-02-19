c4fc27b4792b778a0be760677172bd9deec1f13f
James McLean <james@mcleanlabs.ca>
2015-02-19 14:05:53 -0500

    Added first test case.

--- test/client.js
+++ test/client.js
@@ -1,8 +1,32 @@
 var expect = require('expect.js');
+var emitter = require('component-emitter');
 
-describe('tests', function () {
-	it('should be true', function (done) {
-		expect(true).to.be(true);
-		done();
+var Client = require('../lib/client');
+
+function MockSocket () {
+}
+
+emitter(MockSocket.prototype);
+
+describe('client', function () {
+	var socket, client;
+	
+	beforeEach(function () {
+		socket = new MockSocket();
+		client = new Client(socket);
+	});
+	
+	it('should request service list and handle response', function (done) {
+		var response = { id: 0, type: 'services', services: ['TestService1', 'TestService2'] };
+		
+		socket.send = function (object) {
+			expect(object).to.eql({ id: 0, type: 'services' });
+			socket.emit('message', JSON.stringify(response));
+		};
+		
+		client.services().done(function (result) {
+			expect(result).to.eql(response.services);
+			done();
+		});
 	});
 });
