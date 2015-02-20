46ce85f77ee90c32af05fdee36181d8ffdc7ca82
James McLean <james@mcleanlabs.ca>
2015-02-20 14:50:45 -0500

    Improved error handling. Added more test cases.

--- test/client.js
+++ test/client.js
@@ -14,31 +14,97 @@ describe('client', function () {
 		client = io.remoting(socket);
 	});
 	
-	it('should request service list and handle response', function (done) {
-		var response = { id: 0, type: 'services', services: ['TestService1', 'TestService2'] };
+	describe('services', function () {
+		it('should request service list and handle response', function (done) {
+			var response = { id: 0, type: 'services', services: ['TestService1', 'TestService2'] };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'services' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.services().done(function (result) {
+				expect(result).to.eql(response.services);
+				done();
+			});
+		});
 		
-		socket.send = function (object) {
-			expect(object).to.eql({ id: 0, type: 'services' });
-			socket.emit('message', JSON.stringify(response));
-		};
+		it('should handle error response', function (done) {
+			var response = { id: 0, type: 'error', name: 'Error', message: 'Test error' };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'services' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.services().done(function () {
+			}, function (error) {
+				expect(error.name).to.be(response.name);
+				expect(error.message).to.be(response.message);
+				done();
+			});
+		});
 		
-		client.services().done(function (result) {
-			expect(result).to.eql(response.services);
-			done();
+		it('should handle bad response type', function (done) {
+			var response = { id: 0, type: 'bad' };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'services' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.services().done(function () {
+			}, function (error) {
+				expect(error.name).to.be('InvalidResponse');
+				done();
+			});
 		});
 	});
 	
-	it('should request exports list and handle response', function (done) {
-		var response = { id: 0, type: 'exports', exports: ['test1', 'test2'] };
+	describe('exports', function () {
+		it('should request exports list and handle response', function (done) {
+			var response = { id: 0, type: 'exports', exports: ['test1', 'test2'] };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.exports('TestService').done(function (result) {
+				expect(result).to.eql(response.exports);
+				done();
+			});
+		});
 		
-		socket.send = function (object) {
-			expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
-			socket.emit('message', JSON.stringify(response));
-		};
+		it('should handle error response', function (done) {
+			var response = { id: 0, type: 'error', name: 'Error', message: 'Test error' };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.exports('TestService').done(function () {
+			}, function (error) {
+				expect(error.name).to.be(response.name);
+				expect(error.message).to.be(response.message);
+				done();
+			});
+		});
 		
-		client.exports('TestService').done(function (result) {
-			expect(result).to.eql(response.exports);
-			done();
+		it('should handle bad response type', function (done) {
+			var response = { id: 0, type: 'bad' };
+			
+			socket.send = function (object) {
+				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
+				socket.emit('message', JSON.stringify(response));
+			};
+			
+			client.exports('TestService').done(function () {
+			}, function (error) {
+				expect(error.name).to.be('InvalidResponse');
+				done();
+			});
 		});
 	});
 });
