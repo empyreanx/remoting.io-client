ae2e740702d49826908340f5dd778dcb86e3dafc
James McLean <james@mcleanlabs.ca>
2015-02-20 19:50:36 -0500

    Removed redundant tests.

--- test/client.js
+++ test/client.js
@@ -61,54 +61,17 @@ describe('client', function () {
 		});
 	});
 	
-	describe('exports', function () {
-		it('should request exports list and handle response', function (done) {
-			var response = { id: 0, type: 'exports', result: ['test1', 'test2'] };
-			
-			socket.send = function (object) {
-				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
-				socket.emit('message', JSON.stringify(response));
-			};
-			
-			client.exports('TestService').done(function (result) {
-				expect(result).to.eql(response.result);
-				done();
-			});
-		});
+	it('should request exports list and handle response', function (done) {
+		var response = { id: 0, type: 'exports', result: ['test1', 'test2'] };
 		
-		it('should handle error response', function (done) {
-			var response = { id: 0, type: 'error', name: 'Error', message: 'Test error' };
-			
-			socket.send = function (object) {
-				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
-				socket.emit('message', JSON.stringify(response));
-			};
-			
-			client.exports('TestService').done(function () {
-			}, function (error) {
-				expect(error.name).to.be(response.name);
-				expect(error.message).to.be(response.message);
-				done();
-			});
-		});
+		socket.send = function (object) {
+			expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
+			socket.emit('message', JSON.stringify(response));
+		};
 		
-		it('should handle bad response type', function (done) {
-			var response = { id: 0, type: 'bad' };
-			
-			socket.send = function (object) {
-				expect(object).to.eql({ id: 0, type: 'exports', service: 'TestService' });
-				socket.emit('message', JSON.stringify(response));
-			};
-			
-			client.exports('TestService').done(function () {
-			}, function (error) {
-				expect(error.name).to.be('InvalidResponse');
-				done();
-			});
+		client.exports('TestService').done(function (result) {
+			expect(result).to.eql(response.result);
+			done();
 		});
-	});
-	
-	describe('create', function () {
-		
 	});
 });
